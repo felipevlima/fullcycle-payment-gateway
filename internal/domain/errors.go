a76132febebab3578fbf91f4a6753c9a19859b95
Felipe Vieira Lima <felipe.lima@inter.co>
2025-04-09 12:49:34 -0300

    feat: add invoice.go and update InvoiceRepository on repository.go

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -7,4 +7,6 @@ var (
 	ErrDuplicatedApiKey   = errors.New("Api key already exists")
 	ErrInvoiceNotFound    = errors.New("Invoice not found")
 	ErrUnauthorizedAccess = errors.New("Unauthorized access")
+	ErrInvalidAmount      = errors.New("Invalid amount")
+	ErrInvalidStatus      = errors.New("Invalid status")
 )
