9f808775da415c880179d1005dfb86eadc149875
Felipe Vieira Lima <felipe.lima@inter.co>
2025-04-09 12:50:13 -0300

    feat: add invoice.go and update InvoiceRepository on repository.go

--- internal/domain/respository.go
+++ internal/domain/respository.go
@@ -10,5 +10,6 @@ type AccountRepository interface {
 type InvoiceRepository interface {
 	Save(invoice *Invoice) error
 	FindByID(id string) (*Invoice, error)
+	FindByAccountID(accountID string) ([]*Invoice, error)
 	UpdateStatus(invoice *Invoice) error
 }
